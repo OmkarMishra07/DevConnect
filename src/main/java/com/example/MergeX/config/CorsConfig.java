19561c21be9ab87ffcdb5b1e5be090850d51349c
Omkar Mishra <142795973+OmkarMishra07@users.noreply.github.com>
2026-05-19 22:03:31 +0530

    Update allowed origins in CORS configuration

--- src/main/java/com/example/MergeX/config/CorsConfig.java
+++ src/main/java/com/example/MergeX/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://project-pva0r.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
